5d55f1543115382eae088f8e80a5ef72a15b4a31
phitruongsgu <truong30112000@gmail.com>
2022-11-23 02:41:57 +0700

    fluentValidation + AdminApp Layout

---
+++ eIMIC223925.AdminApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eIMIC223925.AdminApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- eIMIC223925.ViewModels/System/Users/LoginRequest.cs
+++ eIMIC223925.ViewModels/System/Users/LoginRequest.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace eIMIC223925.ViewModels.System.Users
+﻿namespace eIMIC223925.ViewModels.System.Users
 {
     public class LoginRequest
     {
+        //[Required]
         public string UserName { get; set; }
 
+        //[Required]
         public string Password { get; set; }
 
         public bool RememberMe { get; set; }
